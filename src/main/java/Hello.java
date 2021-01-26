d59f1890bf5f7077e055bdb6ee0fe936d6e275d6
Aibek-21 <aibek61@gmail.com>
2021-01-26 12:26:16 -0800

    some more changes on movie 1 branch

--- src/main/java/Hello.java
+++ src/main/java/Hello.java
@@ -1,8 +1,10 @@
 public class Hello {
     public static void main(String[] args) {
 
-        System.out.println("Hello World, B21");
+        System.out.println("Hello World");
         System.out.println("B21 future SDET learning GIT!");
-        //watch your movie in bedroom
+        //we did more changes on master branch
+        //we made some more changes
+
     }
 }
