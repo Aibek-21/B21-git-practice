8880692160d2d5708dd4bb596a1fc07e9c18ad5c
Aibek-21 <aibek61@gmail.com>
2021-01-26 12:38:01 -0800

    movie 2 branch watching in living room

--- src/main/java/Hello.java
+++ src/main/java/Hello.java
@@ -4,5 +4,6 @@ public class Hello {
         System.out.println("Hello World");
         System.out.println("B21 future SDET learning GIT!");
         //we did more changes on master branch
+        //movie on the phone
     }
 }
