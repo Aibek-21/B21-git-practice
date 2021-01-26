0875da421c81a4a0e076bebdec6fb1f99dc03beb
Aibek-21 <aibek61@gmail.com>
2021-01-26 12:39:47 -0800

    master branch watching movie for 10 min

--- src/main/java/Hello.java
+++ src/main/java/Hello.java
@@ -4,5 +4,6 @@ public class Hello {
         System.out.println("Hello World");
         System.out.println("B21 future SDET learning GIT!");
         //we did more changes on master branch
+        //ohh, i like this movie
     }
 }
